b7f5e487f2a8508ca8761b8ef39528e063a1991d
VirtusStack <payalgupta6403@gmail.com>
2025-09-12 13:29:04 +0530

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<?php
+// includes/header.php
+
+// Common header included on all pages
+// Contains <head> section and opening <body> and <header>
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Property Amenity Management System</title>
+    <!-- Link to external CSS -->
+    <link rel="stylesheet" href="../public/css/style.css">
+
+</head>
+<body>
+<header>
+    <!-- Site title / branding -->
+    <h1>Property Amenity Management System</h1>
+</header>
