6d27ac83d3fd498b5e80193384a6479daa3bd501
VirtusStack <payalgupta6403@gmail.com>
2025-09-12 13:32:37 +0530

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<?php
+// includes/footer.php
+
+// Common footer included on all pages
+// Contains closing <body> and <html> tags
+?>
+<footer>
+    <!-- Footer text -->
+    <p>&copy; <?php echo date("Y"); ?> Property Amenity Management System. All rights reserved.</p>
+</footer>
+</body>
+</html>
