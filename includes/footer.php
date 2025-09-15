beb980793770c2d14f4fc7f2963aaac1e14f86aa
VirtusStack <payalgupta6403@gmail.com>
2025-09-15 13:25:36 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -8,5 +8,8 @@
     <!-- Footer text -->
     <p>&copy; <?php echo date("Y"); ?> Property Amenity Management System. All rights reserved.</p>
 </footer>
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
