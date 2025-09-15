2b2309b8ea046106f62492d31385346afab01ab0
VirtusStack <payalgupta6403@gmail.com>
2025-09-15 13:25:56 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -9,14 +9,17 @@
 <head>
     <meta charset="UTF-8">
     <title>Property Amenity Management System</title>
-    <!-- Link to external CSS -->
+    <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+      <!-- FontAwesome for icons -->
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
+     <!-- Link to external CSS -->
      <link rel="stylesheet" href="<?= BASE_URL ?>/public/css/style.css">
-
-
-
+    
+  
 </head>
 <body>
 <header>
-    <!-- Site title / branding -->
+      <!-- Site title / branding -->
     <h1>Property Amenity Management System</h1>
-</header>
+   </header>
