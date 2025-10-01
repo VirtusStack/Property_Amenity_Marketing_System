2795df00e0fa3659619426b1654c4960bb06d198
VirtusStack <payalgupta6403@gmail.com>
2025-10-01 11:31:58 +0530

    Update edit_user.php

--- templates/users/edit_user.php
+++ templates/users/edit_user.php
@@ -34,8 +34,7 @@
                     <div class="alert <?= strpos($results['message'],'✅')!==false ? 'alert-success' : 'alert-danger' ?> alert-dismissible fade show" role="alert">
                      <?= strpos($results['message'],'✅')!==false ? '<i class="fas fa-check-circle"></i>' : '<i class="fas fa-times-circle"></i>' ?>
                         <?= htmlspecialchars($results['message']) ?>
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
+                                           </div>
                 <?php endif; ?>
 
                 <!-- User Edit Form Card -->
