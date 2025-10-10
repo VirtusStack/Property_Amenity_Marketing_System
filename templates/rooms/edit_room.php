c9772a0c6741f9bd468a6e2ef12983002d93f4ce
VirtusStack <payalgupta6403@gmail.com>
2025-10-10 10:56:27 +0530

    Update edit_room.php

--- templates/rooms/edit_room.php
+++ templates/rooms/edit_room.php
@@ -1,5 +1,83 @@
 <?php
 // /templates/rooms/edit_room.php
+// Facility Groups with Icons
+
+$facilityGroups = [
+    'Climate & Comfort' => [
+        ['name' => 'AC', 'icon' => 'fa-snowflake'],
+        ['name' => 'Fan', 'icon' => 'fa-fan'],
+        ['name' => 'Soundproof Windows', 'icon' => 'fa-window-maximize'],
+        ['name' => 'Hot Water / Geyser', 'icon' => 'fa-tint']
+    ],
+    'Beds & Sleeping' => [
+        ['name' => 'Single Bed', 'icon' => 'fa-bed'],
+        ['name' => 'Double Bed', 'icon' => 'fa-bed'],
+        ['name' => 'Queen Bed', 'icon' => 'fa-bed'],
+        ['name' => 'King Bed', 'icon' => 'fa-bed'],
+        ['name' => 'Extra Bed', 'icon' => 'fa-plus'],
+        ['name' => 'Extra Mattress', 'icon' => 'fa-plus-square'],
+        ['name' => 'Baby Cot / Cradle', 'icon' => 'fa-baby']
+    ],
+    'Bathroom & Toiletries' => [
+        ['name' => 'Attached Bathroom', 'icon' => 'fa-bath'],
+        ['name' => 'Toiletries', 'icon' => 'fa-pump-soap'],
+        ['name' => 'Towels & Bathrobe', 'icon' => 'fa-tshirt'],
+        ['name' => 'Hair Dryer', 'icon' => 'fa-wind']
+    ],
+    'Entertainment & Connectivity' => [
+        ['name' => 'LED TV', 'icon' => 'fa-tv'],
+        ['name' => 'Cable Channels', 'icon' => 'fa-satellite-dish'],
+        ['name' => 'Smart TV / Netflix', 'icon' => 'fa-film'],
+        ['name' => 'Music System', 'icon' => 'fa-music'],
+        ['name' => 'Free Wi-Fi', 'icon' => 'fa-wifi'],
+        ['name' => 'Intercom', 'icon' => 'fa-phone']
+    ],
+    'Work & Safety' => [
+        ['name' => 'Work Desk', 'icon' => 'fa-chair'],
+        ['name' => 'Safe Locker', 'icon' => 'fa-lock'],
+        ['name' => 'Key Card Access', 'icon' => 'fa-key'],
+        ['name' => 'CCTV Security', 'icon' => 'fa-video'],
+        ['name' => 'Fire Extinguisher', 'icon' => 'fa-fire-extinguisher'],
+        ['name' => 'Smoke Detector', 'icon' => 'fa-smog']
+    ],
+    'Views & Outdoor' => [
+        ['name' => 'Private Balcony', 'icon' => 'fa-umbrella-beach'],
+        ['name' => 'Sea View', 'icon' => 'fa-water'],
+        ['name' => 'Pool View', 'icon' => 'fa-swimming-pool'],
+        ['name' => 'Garden View', 'icon' => 'fa-leaf'],
+        ['name' => 'City View', 'icon' => 'fa-city'],
+        ['name' => 'Mountain View', 'icon' => 'fa-mountain'],
+        ['name' => 'Sofa / Sitting Area', 'icon' => 'fa-couch']
+    ],
+    'Services & Amenities' => [
+        ['name' => 'Lift Access', 'icon' => 'fa-elevator'],
+        ['name' => 'Parking Available', 'icon' => 'fa-car'],
+        ['name' => 'Power Backup', 'icon' => 'fa-bolt'],
+        ['name' => 'Laundry Service', 'icon' => 'fa-soap'],
+        ['name' => 'Luggage Storage', 'icon' => 'fa-suitcase'],
+        ['name' => 'Room Service', 'icon' => 'fa-bell'],
+        ['name' => 'Daily Housekeeping', 'icon' => 'fa-broom'],
+        ['name' => 'Tea/Coffee Maker', 'icon' => 'fa-mug-hot'],
+        ['name' => 'Welcome Drink', 'icon' => 'fa-glass-cheers'],
+        ['name' => 'Breakfast Included', 'icon' => 'fa-bread-slice'],
+        ['name' => 'Complimentary Water', 'icon' => 'fa-tint']
+    ],
+    'Leisure & Extras' => [
+        ['name' => 'Swimming Pool', 'icon' => 'fa-swimming-pool'],
+        ['name' => 'Gym / Fitness', 'icon' => 'fa-dumbbell'],
+        ['name' => 'Spa & Wellness', 'icon' => 'fa-spa'],
+        ['name' => 'Play Area', 'icon' => 'fa-child'],
+        ['name' => 'Restaurant', 'icon' => 'fa-utensils'],
+        ['name' => 'Bar', 'icon' => 'fa-cocktail'],
+        ['name' => 'Conference Room', 'icon' => 'fa-users'],
+        ['name' => 'Banquet Hall', 'icon' => 'fa-hotel']
+    ],
+    'Policies' => [
+        ['name' => 'Pet Friendly', 'icon' => 'fa-dog'],
+        ['name' => 'Smoking Allowed', 'icon' => 'fa-smoking'],
+        ['name' => 'Non-Smoking Room', 'icon' => 'fa-ban']
+    ]
+];
 
 require_once __DIR__ . '/../../config/config.php';
 ?>
@@ -128,22 +206,37 @@ require_once __DIR__ . '/../../config/config.php';
                                 </select>
                             </div>
 
-                            <!-- Facilities -->
-                            <div class="form-group mb-3">
-                                <label>Facilities:</label>
-                                <div class="row">
-                                    <?php foreach ($results['facilities'] as $facility): ?>
-                                        <div class="col-md-3">
-                                            <div class="form-check">
-                                                <input type="checkbox" name="facilities[]" value="<?= $facility['facility_id'] ?>"
-                                                    class="form-check-input"
-                                                    <?= in_array($facility['facility_id'], $results['room']['facilities']) ? 'checked' : '' ?>>
-                                                <label class="form-check-label"><?= htmlspecialchars($facility['name']) ?></label>
-                                            </div>
-                                        </div>
-                                    <?php endforeach; ?>
-                                </div>
-                            </div>
+                           <!-- Facilities -->
+			 <div class="form-group mb-4">
+    			<label>Facilities / Room Features:</label>
+
+    			<?php foreach ($facilityGroups as $group => $items): ?>
+        		  <h6 class="mt-3"><strong><?= htmlspecialchars($group) ?></strong></h6>
+       			 <div class="row">
+           		 <?php foreach ($items as $facility): ?>
+                	<?php 
+                 	// If editing, preselect facility if it exists in $results['facilities_selected']
+                	$checked = (isset($results['facilities_selected']) && in_array($facility['name'], $results['facilities_selected'])) 
+                    	? 'checked' 
+                    	: '';
+               		 ?>
+                	<div class="col-md-4">
+                    	<div class="form-check">
+                        <input type="checkbox" class="form-check-input" 
+                               name="facilities[]" 
+                               value="<?= htmlspecialchars($facility['name']) ?>" 
+                               id="facility<?= md5($facility['name']) ?>" 
+                               <?= $checked ?>>
+                        	<label class="form-check-label" for="facility<?= md5($facility['name']) ?>">
+                            	<i class="fas <?= $facility['icon'] ?>"></i>
+                            	<?= htmlspecialchars($facility['name']) ?>
+                        	</label>
+                   	        </div>
+                		</div>
+            			<?php endforeach; ?>
+       			      </div>
+   		 	   <?php endforeach; ?>
+			</div>
 
                             <button type="submit" class="btn btn-primary">Update Room</button>
                             <a href="<?= BASE_URL ?>/admin.php?action=manageRooms" class="btn btn-secondary">Cancel</a>
