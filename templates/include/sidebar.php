e737f05d6b9308c8d8cd3c0b5640357fab1e7935
VirtusStack <payalgupta6403@gmail.com>
2025-10-18 18:20:04 +0530

    Update sidebar.php

--- templates/include/sidebar.php
+++ templates/include/sidebar.php
@@ -1,3 +1,28 @@
+<?php
+// /templates/include/sidebar.php
+// -------------------------
+// Sidebar template with dynamic plugin integration
+
+global $pdo;
+
+$location_id = $_SESSION['location_id'] ?? null;
+
+//  Auto-pick first location if none selected (for admin)
+if (!$location_id && isset($pdo)) {
+    $stmt = $pdo->query("SELECT location_id FROM locations WHERE is_deleted = 0 LIMIT 1");
+    $location_id = $stmt->fetchColumn();
+    $_SESSION['location_id'] = $location_id;
+}
+
+$enabledPlugins = [];
+
+if ($location_id && isset($pdo)) {
+    $enabledPlugins = Plugin::getEnabled($pdo, $location_id);
+}
+
+
+?>
+
 <!-- Sidebar -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
@@ -9,10 +34,9 @@
     <div class="sidebar-brand-text mx-3">Property System</div>
   </a>
 
-  <!-- Divider -->
   <hr class="sidebar-divider my-0">
 
-  <!-- Nav Item - Dashboard -->
+  <!-- Dashboard -->
   <li class="nav-item active">
     <a class="nav-link" href="<?= BASE_URL ?>/admin.php">
       <i class="fas fa-fw fa-tachometer-alt"></i>
@@ -20,21 +44,17 @@
     </a>
   </li>
 
-  <!-- Divider -->
   <hr class="sidebar-divider">
 
-  <!-- Heading -->
-  <div class="sidebar-heading">
-    Management
-  </div>
+  <div class="sidebar-heading">Management</div>
 
-  <!-- Users Menu -->
+  <!-- Users -->
   <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUsers" aria-expanded="true" aria-controls="collapseUsers">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUsers">
       <i class="fas fa-users"></i>
       <span>Users</span>
     </a>
-    <div id="collapseUsers" class="collapse" aria-labelledby="headingUsers" data-parent="#accordionSidebar">
+    <div id="collapseUsers" class="collapse" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
         <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=manageUsers">Manage Users</a>
         <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=newUser">Add User</a>
@@ -42,13 +62,13 @@
     </div>
   </li>
 
-  <!-- Roles Menu -->
+  <!-- Roles -->
   <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseRoles" aria-expanded="true" aria-controls="collapseRoles">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseRoles">
       <i class="fas fa-user-tag"></i>
       <span>Roles</span>
     </a>
-    <div id="collapseRoles" class="collapse" aria-labelledby="headingRoles" data-parent="#accordionSidebar">
+    <div id="collapseRoles" class="collapse" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
         <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=manageRoles">Manage Roles</a>
         <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=newRole">Add Role</a>
@@ -56,13 +76,13 @@
     </div>
   </li>
 
-  <!-- Companies Menu -->
+  <!-- Companies -->
   <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseCompanies" aria-expanded="true" aria-controls="collapseCompanies">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseCompanies">
       <i class="fas fa-city"></i>
       <span>Companies</span>
     </a>
-    <div id="collapseCompanies" class="collapse" aria-labelledby="headingCompanies" data-parent="#accordionSidebar">
+    <div id="collapseCompanies" class="collapse" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
         <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=manageCompanies">Manage Companies</a>
         <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=newCompany">Add Company</a>
@@ -70,13 +90,13 @@
     </div>
   </li>
 
-  <!-- Locations Menu -->
+  <!-- Locations -->
   <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLocations" aria-expanded="true" aria-controls="collapseLocations">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLocations">
       <i class="fas fa-map-marker-alt"></i>
       <span>Locations</span>
     </a>
-    <div id="collapseLocations" class="collapse" aria-labelledby="headingLocations" data-parent="#accordionSidebar">
+    <div id="collapseLocations" class="collapse" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
         <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=manageLocations">Manage Locations</a>
         <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=newLocation">Add Location</a>
@@ -84,36 +104,73 @@
     </div>
   </li>
 
-  <!-- Rooms Menu -->
-<li class="nav-item">
-  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseRooms" aria-expanded="true" aria-controls="collapseRooms">
-    <i class="fas fa-door-open"></i>
-    <span>Rooms</span>
-  </a>
-  <div id="collapseRooms" class="collapse" aria-labelledby="headingRooms" data-parent="#accordionSidebar">
-    <div class="bg-white py-2 collapse-inner rounded">
-      <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=manageRooms">Manage Rooms</a>
-      <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=newRoom">Add Room</a>
+  <!-- Rooms -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseRooms">
+      <i class="fas fa-door-open"></i>
+      <span>Rooms</span>
+    </a>
+    <div id="collapseRooms" class="collapse" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=manageRooms">Manage Rooms</a>
+        <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=newRoom">Add Room</a>
+      </div>
     </div>
-  </div>
-</li>
+  </li>
 
-<!-- Restaurants Menu -->
-<li class="nav-item">
-  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseRestaurants" aria-expanded="true" aria-controls="collapseRestaurants">
-    <i class="fas fa-utensils"></i>
-    <span>Restaurants</span>
-  </a>
-  <div id="collapseRestaurants" class="collapse" aria-labelledby="headingRestaurants" data-parent="#accordionSidebar">
-    <div class="bg-white py-2 collapse-inner rounded">
-      <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=manageRestaurant">Manage Restaurants</a>
-      <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=newRestaurant">Add Restaurant</a>
+  <hr class="sidebar-divider">
+
+  <!-- Dynamic Plugin Menus -->
+  <?php if (!empty($enabledPlugins)): ?>
+      <div class="sidebar-heading">Enabled Features</div>
+      <?php 
+      $icons = [
+          'Restaurant' => 'fa-utensils',
+          'Swimming Pool' => 'fa-swimmer',
+          'Parking' => 'fa-parking',
+          'Spa' => 'fa-spa',
+          'Gym' => 'fa-dumbbell',
+          'Play Area' => 'fa-child'
+      ];
+      ?>
+      <?php foreach ($enabledPlugins as $plugin): ?>
+          <?php 
+          $pluginName = htmlspecialchars($plugin['name']);
+          $pluginKey  = strtolower(str_replace(' ', '', $plugin['name']));
+          $icon = $icons[$plugin['name']] ?? 'fa-plug';
+          ?>
+          <li class="nav-item">
+              <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse<?= $pluginKey ?>">
+                  <i class="fas <?= $icon ?>"></i>
+                  <span><?= $pluginName ?></span>
+              </a>
+              <div id="collapse<?= $pluginKey ?>" class="collapse" data-parent="#accordionSidebar">
+                  <div class="bg-white py-2 collapse-inner rounded">
+                      <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=manage<?= ucfirst($pluginKey) ?>s">
+                          Manage <?= $pluginName ?>s
+                      </a>
+                      <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=new<?= ucfirst($pluginKey) ?>">
+                          Add <?= $pluginName ?>
+                      </a>
+                  </div>
+              </div>
+          </li>
+      <?php endforeach; ?>
+  <?php endif; ?>
+
+  <!-- Plugins Menu -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePlugins">
+      <i class="fas fa-plug"></i>
+      <span>Plugins</span>
+    </a>
+    <div id="collapsePlugins" class="collapse" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=managePlugins">Manage Plugins</a>
+      </div>
     </div>
-  </div>
-</li>
+  </li>
 
-  <!-- Divider -->
   <hr class="sidebar-divider d-none d-md-block">
-
 </ul>
 <!-- End of Sidebar -->
